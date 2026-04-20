765257462ff29c7575bfabe23f2a4e6b2458d960
vkp000 <vivekprajapatvkp0@gmail.com>
2026-04-20 11:58:54 +0530

    feat: complete billing software - backend + frontend

--- reactFrontend/billingSoftwareFrontend/src/Util/Constants.js
+++ reactFrontend/billingSoftwareFrontend/src/Util/Constants.js
@@ -2,5 +2,5 @@
 // Test mode: rzp_test_XXXX | Live: rzp_live_XXXX
 export const AppConstants = {
     RAZORPAY_KEY_ID: "rzp_test_SfDLfEO6CzGf6p",
-    BASE_URL: "http://192.168.137.1:8080"
+    BASE_URL: "http://localhost:8080"
 }
